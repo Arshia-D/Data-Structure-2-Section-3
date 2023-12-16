0091fadaaace38da2b5e74ed9f6166bf4e396b78
Arshia Darvish <107769294+Arshia-D@users.noreply.github.com>
2023-12-16 12:30:00 +0330

    Add files via upload

---
+++ Coin_Combinator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Possible_coin_combinations
+{
+    internal class Coin_Combinator
+    {
+        private static readonly double[] Coins = { 0.01, 0.02, 0.05, 0.10, 0.20, 0.50, 1.00, 2.00 };
+        private List<List<double>> combinations = new List<List<double>>();
+
+        private void FindCombinations(List<double> current, double amount, int index)
+        {
+            if (amount == 0 && current.Count <= 10)
+            {
+                combinations.Add(new List<double>(current));
+                return;
+            }
+
+            if (amount < 0 || current.Count > 10 || index >= Coins.Length)
+            {
+                return;
+            }
+
+            for (int i = index; i < Coins.Length; i++)
+            {
+                current.Add(Coins[i]);
+                FindCombinations(current, amount - Coins[i], i);
+              
+                current.RemoveAt(current.Count - 1);
+            }
+        }
+
+        public List<List<double>> GetAllCombinations(double amount)
+        {
+            if (amount <= 0 || amount > 5.00)
+            {
+                return new List<List<double>>();
+            }
+
+            combinations.Clear();
+            FindCombinations(new List<double>(), amount, 0);
+            return combinations;
+        }
+
+        public List<double> GetMinimumCoinCombination(double amount)
+        {
+            return GetAllCombinations(amount).OrderBy(c => c.Count).FirstOrDefault();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Possible_coin_combinations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double amount = 0;
+                bool isValidInput = false;
+
+                while (!isValidInput)
+                {
+                    Console.Write("Enter an amount (not bigger than 5.00): ");
+                    string input = Console.ReadLine();
+
+                    if (string.IsNullOrEmpty(input))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("No input provided.");
+                        Console.ResetColor();
+                        continue;
+                    }
+
+                    if (!double.TryParse(input, NumberStyles.Any, CultureInfo.InvariantCulture, out amount))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Invalid input. Please enter a valid number.");
+                        Console.ResetColor();
+                        continue;
+                    }
+
+                    if (amount < 0 || amount > 5.00)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Please enter an amount between 0 and 5.00.");
+                        Console.ResetColor();
+                        continue;
+                    }
+
+                    isValidInput = true;
+                }
+
+                Coin_Combinator coinCombinator = new Coin_Combinator();
+                List<double> minCoinsCombination = coinCombinator.GetMinimumCoinCombination(amount);
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("All combinations:");
+                Console.ResetColor();
+
+                foreach (var combination in coinCombinator.GetAllCombinations(amount))
+                {
+                    Console.WriteLine(string.Join(", ", combination));
+                }
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nCombination with the smallest number of coins:");
+                Console.ResetColor();
+
+                if (minCoinsCombination != null && minCoinsCombination.Count > 0)
+                {
+                    Console.WriteLine(string.Join(", ", minCoinsCombination));
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("No combination found.");
+                    Console.ResetColor();
+                }
+
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Press Enter to exit the application.");               
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                Console.ResetColor();
+
+                Console.WriteLine("Press Enter to exit the application.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
